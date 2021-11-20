66ff6693f778e4d36e8c478f5f8038bf574a5e68
KCH6937 <adasddasd12@naver.com>
2021-11-21 04:20:27 +0900

    initial commit - 프로젝트 초기 세팅 :

---
+++ EngineerInformationProcessingGuide/Controller/FavoriteViewController.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+class FavoriteViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
---
+++ EngineerInformationProcessingGuide/Controller/InfoViewController.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+class InfoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
