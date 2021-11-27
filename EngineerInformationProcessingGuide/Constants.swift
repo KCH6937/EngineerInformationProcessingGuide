a8db629514749e7e34e61afb39396f503fd61aa4
KCH6937 <adasddasd12@naver.com>
2021-11-27 17:22:19 +0900

    feat: API 관련 코드

---
+++ EngineerInformationProcessingGuide/Constants.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+struct APIKey {
+    static let NAVER_ID = "mpW7rBCSaXqg6yRyp6aE"
+    static let NAVER_SECRET = "cvcarhA1Rp"
+    
+}
+
+struct EndPoint {
+    static let searchURL = "https://openapi.naver.com/v1/search/"
+}
